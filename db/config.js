86a36e6352b418f3b00f731b0005e27119f85ef5
Jacob Meltzer <jmeltzer33@gmail.com>
2017-07-31 15:12:21 -0400

    wrote config js file

---
+++ db/config.js
@@ -0,0 +1,17 @@
+const pgp = require('pg-promise')();
+
+function setDatabase () {
+  if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
+    return pgp({
+      database: 'got_info_dev',
+      port: 5432,
+      host: 'locahost',
+    });
+  } else if (process.env.NODE_ENV === 'production') {
+    return pgp(process.env.DATABASE_URL);
+  }
+}
+
+const db = setDatabase();
+
+module.exports = db;
