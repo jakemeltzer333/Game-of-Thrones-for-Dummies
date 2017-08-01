a8d6132dcc21d4f49d596859c3836c46c96abe33
Jacob Meltzer <jmeltzer33@gmail.com>
2017-08-01 08:52:08 -0400

    updated app js file to include stuff for auth

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const logger = require('morgan');
 const path = require('path');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const passport = require ('passport');
 
 const app = express();
 
@@ -12,6 +15,14 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(methodOverride('_method'));
+app.use(cookieParser());
+app.use(session({
+  secret: process.env.SECRET_KEY,
+  resave: false,
+  saveUninitialized: true,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(express.static('public'));
 
@@ -34,10 +45,10 @@ app.get('/', (req,res) => {
 
 const gotRoutes = require('./routes/got-routes');
 app.use('/got', gotRoutes);
-// const authRoutes = require('./routes/auth-routes');
-// app.use('/auth', authRoutes);
-// const userRoutes = require('./routes/user-routes');
-// app.use('/user', userRoutes);
+const authRoutes = require('./routes/auth-routes');
+app.use('/auth', authRoutes);
+const userRoutes = require('./routes/user-routes');
+app.use('/user', userRoutes);
 
 app.get('*', (req, res) => {
   res.status(400).send('Page Not Found');
