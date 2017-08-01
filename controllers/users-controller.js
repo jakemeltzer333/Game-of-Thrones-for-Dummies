fa5f7b4f6cfe07898a398a2c3ea3c40e0f100326
Jacob Meltzer <jmeltzer33@gmail.com>
2017-08-01 09:42:48 -0400

    filled out users controller file

---
+++ controllers/users-controller.js
@@ -0,0 +1,34 @@
+const bcrypt = require('bcryptjs');
+const User = require('../models/user.js');
+
+const usersController = {};
+
+usersController.index = (req, res) => {
+  User.findUserGot(req.user.id)
+    .then(got => {
+      res.redirect('/got');
+    }).catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+}
+
+usersController.create = (req, res) => {
+  const salt = bcrypt.genSaltSync();
+  const hash = bcrypt.hashSync(req.body.password, salt);
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password_digest: hash,
+  }).then(user => {
+    req.login(user, (err) => {
+      if (err) return next(err);
+      res.redirect('/user');
+    });
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json({error: err});
+  });
+}
+
+module.exports = usersController;
