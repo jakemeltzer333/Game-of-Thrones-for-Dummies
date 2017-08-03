fb23c63a4afd03b6f389a19e4784ee44604a0b38
Jacob Meltzer <jmeltzer33@gmail.com>
2017-08-03 13:58:06 -0400

    altered data table and api call

--- db/migrations/migration-08012017.sql
+++ db/migrations/migration-08012017.sql
@@ -14,10 +14,11 @@ CREATE TABLE IF NOT EXISTS got (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255),
   user_id INTEGER REFERENCES users(id) NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS info (
-  id SERIAL PRIMARY KEY,
-  info_info VARCHAR(255),
-  char_id INTEGER REFERENCES got(id)
+  culture VARCHAR(255),
+  titles VARCHAR(255),
+  aliases VARCHAR(255),
+  father VARCHAR(255),
+  mother VARCHAR(255),
+  allegiances VARCHAR(255),
+  playedBy VARCHAR(255)
 );
--- public/src/main.js
+++ public/src/main.js
@@ -6,14 +6,24 @@ $(() => {
       url: `https://anapioficeandfire.com/api/characters/?name=${seeMore}`,
       method: 'GET',
       success: (data) => {
-        console.log(data);
+        //console.log(data);
+        const got = {
+          culture: data.culture,
+          titles: data.titles,
+          aliases: data.aliases,
+          father: data.father,
+          mother: data.mother,
+          allegiances: data.allegiances,
+          playedBy: data.playedBy
+        }
+        sendToDB(got);
       }
     })
   })
 
   const sendToDB = got => {
     $.ajax({
-      url: '/got',
+      url: '/user',
       method: 'POST',
       data: got
     })
