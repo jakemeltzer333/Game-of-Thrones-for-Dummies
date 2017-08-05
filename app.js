04a6f151a8114156609ba4da1f69bb6eca6ba6bb
Jacob Meltzer <jmeltzer33@gmail.com>
2017-08-05 13:31:48 -0400

    commented my app js file

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//initialize app dependencies
 const express = require('express');
 const logger = require('morgan');
 const path = require('path');
@@ -7,11 +8,13 @@ const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const passport = require ('passport');
 
-
+//initialize the app
 const app = express();
 
+//require dotenv for security
 require('dotenv').config();
 
+//middlewares
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -25,17 +28,20 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+//initialize static file
 app.use(express.static('public'));
 
+//initialize views
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//initialize the local host port to listen on
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
 
-
+//set up index route
 app.get('/', (req,res) => {
   res.render('index', {
     message: 'Game of Thrones For Dummies',
@@ -43,14 +49,14 @@ app.get('/', (req,res) => {
     documentTitle: 'Game of Thrones For Dummies',
   });
 });
-
+//require all the routes for the app
 const gotRoutes = require('./routes/got-routes');
 app.use('/got', gotRoutes);
 const authRoutes = require('./routes/auth-routes');
 app.use('/auth', authRoutes);
 const userRoutes = require('./routes/user-routes');
 app.use('/user', userRoutes);
-
+//error handler
 app.get('*', (req, res) => {
   res.status(400).send('Page Not Found');
 });
